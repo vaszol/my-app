9628b3baac1152dc2a6aa935476bea397db06f95
vaszol <vaszol@mail.ru>
2022-07-16 05:50:31 +0300

    add Routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, FormControl, Form, Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import logo from './logo.png'
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Contacts from '../pages/Contacts';
+import Blog from '../pages/Blog';
 
 export default class Header extends Component {
     render() {
@@ -21,7 +26,7 @@ export default class Header extends Component {
                         <Navbar.Collapse id='responsive-navbar-nav'>
                             <Nav className="me-auto my-2 my-lg-0">
                                 <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/About">About us</Nav.Link>
+                                <Nav.Link href="/about">About us</Nav.Link>
                                 <Nav.Link href="/contacts">Contacts</Nav.Link>
                                 <Nav.Link href="/blog">Blog</Nav.Link>
                             </Nav>
@@ -36,6 +41,14 @@ export default class Header extends Component {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
+                <Router>
+                    <Routes>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/about' component={About} />
+                        <Route exact path='/contacts' component={Contacts} />
+                        <Route exact path='/blog' component={Blog} />
+                    </Routes>
+                </Router>
             </>
         )
     }
